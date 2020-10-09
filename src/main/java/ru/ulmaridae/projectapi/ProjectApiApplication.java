6afdb121bcb9fe07e07c78e126089281d2cf09e7
lmaridae <lmaridae@icloud.com>
2020-10-09 10:31:07 +0300

    Initial commit

---
+++ src/main/java/ru/ulmaridae/projectapi/ProjectApiApplication.java
@@ -0,0 +1,13 @@
+package ru.ulmaridae.projectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ulmaridae/projectapi/ProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ulmaridae.projectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
